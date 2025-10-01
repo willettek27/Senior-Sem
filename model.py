9f87964ad9aa09ad2107108331612967bbb31e01
K. Willette <willettek27@gmail.com>
2025-10-01 17:56:42 +0000

    revised model.py

--- model.py
+++ model.py
@@ -2,45 +2,47 @@
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
-import torch.nn.functional as F
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+from safe_domains import safe_domain_check
 
-
-# Load the model and tokenizer
+# Load tokenizer & model
 model_name = "r3ddkahili/final-complete-malicious-url-model"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 model.eval()
 
-# Mapping prediction to labels
+# Label map 
 label_map = {0: "Benign", 1: "Defacement", 2: "Phishing", 3: "Malware"}
 
-thresholds = {
-    "Defacement": 0.75,
-    "Malware": 0.85,
-    "Phishing": 0.9,
-}
-
-safe_domains = ["wikipedia.org", "github.com", "google.com", "stackoverflow.com", "example.com"]
-
 def predict_model(url: str):
-    if any(domain in url for domain in safe_domains):
+
+    if safe_domain_check(url):
         return {
             "url": url,
-            "prediction": "Benign",
+            "prediction": "Benign"
         }
 
-    inputs = tokenizer([url], return_tensors="pt", truncation=True, padding=True)
+    inputs = tokenizer(url, return_tensors="pt", truncation=True, padding=True)
     with torch.no_grad():
-        logits = model(**inputs).logits
-        probs = F.softmax(logits, dim=1)[0] 
-       
-    passed = {i: probs[i] for i, label in label_map.items()
-        if label != "Benign" and probs[i] >= thresholds.get(label, 0)}
+        outputs = model(**inputs)
+        logits = outputs.logits
+        pred_idx = torch.argmax(logits, dim=1).item()
+        probs = torch.softmax(logits, dim=-1)[0]
+        confidence = probs[pred_idx].item()
 
-    pred_idx = max(passed, key=passed.get) if passed else 0
+    pred_label = label_map[pred_idx]
 
-# === Results ===
     return {
         "url": url,
-        "prediction": label_map[pred_idx] 
+        "prediction": pred_label
     }
+
+def load_dataset():
+   file_path = "malicious_phish.csv"
+   dataset = kagglehub.load_dataset(
+       KaggleDatasetAdapter.PANDAS,
+       "sid321axn/malicious-urls-dataset",
+       file_path
+   )
+   return dataset
