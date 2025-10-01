f6973964cee7546aea9b5ed9965c0b33012e353c
K. Willette <willettek27@gmail.com>
2025-10-01 18:27:20 +0000

    revised safe_domains

--- safe_domains.py
+++ safe_domains.py
@@ -3,14 +3,24 @@
 import tldextract
 
 safe_domains = [
-   "example.com", "google.com", "youtube.com", "facebook.com", "instagram.com", "chatgpt.com",
-    "x.com", "reddit.com", "wikipedia.org", "amazon.com", "tiktok.com",
-    "whatsapp.com", "canva.com", "booking.com", "ebay.com", "duckduckgo.com",
-    "yahoo.com", "telegram.org", "roblox.com", "samsung.com", "mail.ru",
-    "globo.com", "sharepoint.com", "live.com", "office.com", "netflix.com", "linkedin.com",
-    "microsoft.com", "zoom.us", "pinterest.com", "wordpress.com", "adobe.com",
-    "github.com", "stackoverflow.com", "imdb.com", "etsy.com", "bbc.com",
-    "cnn.com", "nytimes.com", "hulu.com", "spotify.com", "dropbox.com"
+   # Popular websites
+    "example.com", "google.com", "youtube.com", "facebook.com", "instagram.com",
+    "chatgpt.com", "x.com", "reddit.com", "wikipedia.org", "amazon.com",
+    "tiktok.com", "whatsapp.com", "canva.com", "booking.com", "ebay.com",
+    "duckduckgo.com", "yahoo.com", "telegram.org", "roblox.com", "samsung.com",
+    "mail.ru", "globo.com", "sharepoint.com", "live.com", "office.com",
+    "netflix.com", "linkedin.com", "microsoft.com", "zoom.us", "pinterest.com",
+    "wordpress.com", "adobe.com", "github.com", "stackoverflow.com", "imdb.com",
+    "etsy.com", "bbc.com", "cnn.com", "nytimes.com", "hulu.com", "spotify.com",
+    "dropbox.com",
+
+    # Educational and governmental sites
+        "harvard.edu", "stanford.edu", "mit.edu", "yale.edu", "princeton.edu",
+        "rockford.edu", "rockvalleycollege.edu"
+    
+    #Canvas
+    "instructure.com"
+
 ]
 
 def safe_domain_check(url: str) -> bool:
